2e2f39b0e10dcd3989b729c39812f8895835bc63
Elian <d2099080@gmail.com>
2024-11-09 16:21:37 +0800

    Implement argmax.s
    Find the index of the maximum element in the array.

--- src/argmax.s
+++ src/argmax.s
@@ -29,8 +29,22 @@ argmax:
 
     li t1, 0
     li t2, 1
+    beq a1, t2, done
 loop_start:
     # TODO: Add your own implementation
+    slli t3, t2, 2
+    add t3, a0, t3
+    lw t4, 0(t3)
+    blt t4, t0, continue
+    add t0, t4, zero
+    add t1, t2, zero
+continue:    
+    addi t2, t2, 1
+    bne t2, a1, loop_start
+done:
+    add a0, t1, zero
+    # Epilogue
+    jr ra  
 
 handle_error:
     li a0, 36
