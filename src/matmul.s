fd7a9ec65491b21249a89a876f824e0655e5e0f8
Elian <d2099080@gmail.com>
2024-11-10 21:13:33 +0800

    Implement matmul.s
    Added 'inner_loop_end' and 'outer_loop_end' blocks to make matmul work
    correctly.

--- src/matmul.s
+++ src/matmul.s
@@ -116,7 +116,25 @@ inner_loop_start:
     
 inner_loop_end:
     # TODO: Add your own implementation
-
+    mv t2, a2
+loop:
+    addi s3, s3, 4
+    addi t2, t2, -1
+    bnez t2, loop    
+    
+    addi s0, s0, 1
+    j outer_loop_start
+outer_loop_end:
+    # Epilogue
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    addi sp, sp, 28
+    jr ra 
 error:
     li a0, 38
     j exit
